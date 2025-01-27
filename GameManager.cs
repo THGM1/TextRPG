e6e5c30f730dc415d7da98491ec89cccdc96aff7
THGM1 <jennypsh@naver.com>
2025-01-27 19:36:48 +0900

    Initial commit

---
+++ GameManager.cs
@@ -0,0 +1,61 @@
+﻿namespace TextRPG
+{
+    internal class GameManager
+    {
+        static string[] menus = { "상태 보기", "인벤토리", "상점" };
+        static Player player;
+        static void Main(string[] args)
+        {
+            Start();
+            
+        }
+        static void Start()
+        {
+            Console.WriteLine("스파르타 마을에 오신 여러분 환영합니다.");
+            Console.WriteLine("이곳에서 던전으로 들어가기전 활동을 할 수 있습니다.\n");
+            if(player == null)
+            {
+                Console.Write("이름을 입력해주세요: ");
+                string name = Console.ReadLine();
+                Console.Write("직업을 입력해주세요: ");
+                string job = Console.ReadLine();
+                player = new Player(name, job);
+            }
+            
+            Menu();
+
+        }
+        static void Menu()
+        {
+            for (int i = 0; i < menus.Length; i++)
+            {
+                Console.WriteLine($"{i + 1}. {menus[i]}");
+            }
+            Console.WriteLine();
+            Console.WriteLine("원하시는 행동을 입력해주세요.");
+            string input = Console.ReadLine();
+            switch (input)
+            {
+                case "1":
+                    ShowStatus();
+                    break;
+                case "2":
+
+                    break;
+            }
+        }
+        static void ShowStatus()
+        {
+            Console.WriteLine();
+            player.getPlayer();
+            Console.WriteLine("\n0. 나가기");
+            Console.WriteLine("\n원하시는 행동을 입력해주세요.");
+            string input = Console.ReadLine();
+            if(input != null)
+            {
+                Menu();
+            }
+
+        }
+    }
+}
---
+++ Player.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextRPG
+{
+    internal class Player
+    {
+        int level; //레벨
+        string name; //이름
+        string job; //직업
+        double atk; //공격력
+        double def; //방어력
+        int hp; //체력
+        int gold; //골드
+
+        public Player(string name, string job)
+        {
+            this.name = name;
+            this.job = job;
+            this.atk = 10;
+            this.def = 5;
+            this.hp = 100;
+            this.gold = 1500;
+        }
+        public void getPlayer()
+        {
+            Console.WriteLine($"Lv. {level}");
+            Console.WriteLine($"{name} ({job})");
+            Console.WriteLine($"공격력: {atk}");
+            Console.WriteLine($"방어력: {def}");
+            Console.WriteLine($"체력: {hp}");
+            Console.WriteLine($"Gold: {gold} G");
+        }
+        
+    }
+}
